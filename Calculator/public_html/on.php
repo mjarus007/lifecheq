b8ab21edbca40dda3f96b6f5715b914470ebe72a
lilit <lilit@196.47.240.90>
2020-01-24 16:31:11 +0200

    HTML: When the calculation data is successfully saved on DB, show a success modal. PHP: connect to the DB and store calc data to table

--- Calculator/public_html/on.php
+++ Calculator/public_html/on.php
@@ -7,10 +7,38 @@ $calcArray = json_decode( $input, true);
 
 
 //Check whether this is from a POST request
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $calcArray[0];
+    $purchase = $calcArray[1];
+    $deposit = $calcArray[2];
+    $year = $calcArray[3];
+    $rate = $calcArray[4];
+    
+    echo "name: $name, $purchase, $deposit, $year, $rate";
+    
+    //Make Database connection
+    $servername = "localhost";
+    $username = "id12331285_lifecheq";
+    $password = "DBPassword";
+    $dbname = "id12331285_calcdb";
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
+    if ($conn->connect_error) {
+        //die("Connection failed: " . $conn->connect_error);
+        echo $conn->connect_error;
+    }
+    
+    //Create SQL Query
+    $sql = "INSERT INTO Calcs (name, purchase, deposit, year, rate) VALUES ('$name', '$purchase', '$deposit', '$year', '$rate')";
 
-//Database:
-//Name: CalcDB
-//username: lifecheq
-//Password: DBPassword
+    //Perfrom SQL Query on Database
+    if( $conn->query( $sql)){
+        echo "Successfully saved";
+    } else{
+        echo "Execution unsuccessful";
+    }
+    
+}
+
 ?>
