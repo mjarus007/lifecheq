a4dbf47c8e16a5ef3d702c9971294f08ea0df66f
lilit <lilit@196.47.240.90>
2020-01-24 15:39:55 +0200

    HTML: Added code to save calculation data on server. PHP: Added code to convert JSON object back to array.

--- Calculator/public_html/on.php
+++ Calculator/public_html/on.php
@@ -1,3 +1,16 @@
 <?php
 
+//
+$input = $_REQUEST["save"];
+
+$calcArray = json_decode( $input, true);
+
+
+//Check whether this is from a POST request
+
+
+//Database:
+//Name: CalcDB
+//username: lifecheq
+//Password: DBPassword
 ?>
